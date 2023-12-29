b1d46ea4e74f9ed67a1ae7b3bbc33099fc1f7fba
OBueckert <owen.bueckert1@gmail.com>
2023-12-29 12:09:45 -0500

    Add windows support

--- app/game/module.mk
+++ app/game/module.mk
@@ -1,5 +1,9 @@
 local_dir := app/game
+ifeq ($(OS),Windows_NT)
+local_prog := $(local_dir)/game.exe
+else
 local_prog := $(local_dir)/game
+endif
 local_src := $(addprefix $(local_dir)/,game.c)
 local_objs := $(subst .c,.o,$(local_src))
 
